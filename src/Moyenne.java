0704e3b954a72a31acbffd3837b2091d7e41da7f
Elise <elise.drimaracci@mines-ales.org>
2022-02-17 10:08:16 +0100

    calcul de moyenne

---
+++ src/Moyenne.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Moyenne {
+    public static void moyenne(float[] coef) {
+        double total = 0;
+        for (int i = 0; i < coef.length; i++) {
+            total = total + coef[i];
+        }
+        double moy = total / coef.length;
+
+        System.out.format("La moyenne est: %.2f", moy);
+    }
+}
