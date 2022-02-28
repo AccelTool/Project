2662c72a63eef35088d0559c8b6834a5f3da0b6c
Elise <elise.drimaracci@mines-ales.org>
2022-02-28 09:19:23 +0100

    calcul de moyenne

--- main/main.java
+++ main/main.java
@@ -1,12 +1,12 @@
 package main;
 
 public class main {
-    public static void moyenne(float[] coef) {
+    public static void moyenne(float[] v) {
         double total = 0;
-        for (int i = 0; i < coef.length; i++) {
-            total = total + coef[i];
+        for (int i = 0; i < v.length; i++) {
+            total = total + v[i];
         }
-        double moy = total / coef.length;
+        double moy = total / v.length;
 
         System.out.format("La moyenne est: %.2f", moy);
     }
