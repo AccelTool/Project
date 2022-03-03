2c7139cfce96cd11e176ab19fb75459c9f72fed6
Hich <hicham.talaoubrid@mines-ales.org>
2022-03-03 11:53:18 +0100

    +Class Bouton

--- src/gui/Bouton.java
+++ src/gui/Bouton.java
@@ -1,4 +1,37 @@
 package gui;
 
+
+import javafx.scene.control.Button;
+
 public class Bouton {
+    Button button;
+
+    public Bouton(double hauteur, double largeur){
+        button = new Button();
+        button.setPrefSize(largeur, hauteur);
+        button.setStyle("");
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public void closeButton(){
+        button.setStyle("");
+    }
+
+    public void reduceButton(){
+        button.setStyle("");
+    }
+
+    public void loginButton(){
+        button.setStyle("");
+    }
+
+    public void createButton(){
+        button.setStyle("");
+    }
+
 }
+
+
