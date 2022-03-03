8e48b21b115a1b8551439ded990002c685938c80
Amine <amine.torki@mines-ales.org>
2022-03-03 11:33:57 +0100

    *Fenetre

--- src/gui/Fenetre.java
+++ src/gui/Fenetre.java
@@ -1,10 +1,13 @@
 package gui;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -20,6 +23,7 @@ public class Fenetre {
         stage.initStyle(StageStyle.UNDECORATED); //Undecorated <=> borderless
         stage.setResizable(false);
         stage.setTitle(name);
+        stage.getIcons().add(new Image(getClass().getResource("/resources/images/logo_acceltool.png").toExternalForm()));
 
         //L'image du background (bg)
         Canvas cv = new Canvas(800, 500);
@@ -31,6 +35,29 @@ public class Fenetre {
         AnchorPane anchorPane = new AnchorPane(cv);
         scene = new Scene(anchorPane, 800, 500);
 
+
+        //Un Pane pour déplacer la fenêtre
+        Pane drag = new Pane(cv);
+        anchorPane.getChildren().add(drag);
+        anchorPane.setTopAnchor(drag,0.0);
+        drag.toFront();
+
+        // Les deux MouseEvent suivants permettent de déplacer la fenêtre car elle est borderless
+        drag.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        drag.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
     }
 
     public Scene getScene() {
