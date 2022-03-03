ce08ae0e89e4f0a3d130d5a5c763bc336c4c8e22
Amine <amine.torki@mines-ales.org>
2022-03-03 11:52:41 +0100

    *Fenetre

--- src/gui/Fenetre.java
+++ src/gui/Fenetre.java
@@ -17,23 +17,33 @@ public class Fenetre {
     private Scene scene;
     private double xOffset = 0;
     private double yOffset = 0;
+    private double width;
+    private double height;
 
     public Fenetre(Stage stage, String name){
 
+
+        width = 1200;
+        height = 600;
         stage.initStyle(StageStyle.UNDECORATED); //Undecorated <=> borderless
         stage.setResizable(false);
         stage.setTitle(name);
-        stage.getIcons().add(new Image(getClass().getResource("/resources/images/logo_acceltool.png").toExternalForm()));
+        String url_logo = getClass().getResource("/resources/images/logo_acceltool.png").toExternalForm();
+        Image im_logo = new Image(url_logo);
+        stage.getIcons().add(im_logo);
+
+        Image rim_logo = new Image(url_logo, 500, 500, false, false);
 
         //L'image du background (bg)
-        Canvas cv = new Canvas(800, 500);
+        Canvas cv = new Canvas(width, height);
         GraphicsContext gc = cv.getGraphicsContext2D();
         gc.setFill(Color.LIGHTSTEELBLUE);
-        gc.fillRect(0,0,800,500);
+        gc.fillRect(0,0,width,height);
+        gc.drawImage(rim_logo,50,50);
 
         // anchorPane correspond au "root"
         AnchorPane anchorPane = new AnchorPane(cv);
-        scene = new Scene(anchorPane, 800, 500);
+        scene = new Scene(anchorPane, width, height);
 
 
         //Un Pane pour déplacer la fenêtre
@@ -41,6 +51,8 @@ public class Fenetre {
         anchorPane.getChildren().add(drag);
         anchorPane.setTopAnchor(drag,0.0);
         drag.toFront();
+
+
 
         // Les deux MouseEvent suivants permettent de déplacer la fenêtre car elle est borderless
         drag.setOnMousePressed(new EventHandler<MouseEvent>() {
